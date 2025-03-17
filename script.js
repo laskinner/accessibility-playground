c56ac6026901f05c8172d093be3367b95292c751
Luke Skinner <laskinner@gmail.com>
2025-03-17 20:22:59 +0800

    Added more elements, flashing lights

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ document.getElementById('counterBtn').addEventListener('mousedown', function() {
     let count = parseInt(document.getElementById('count').innerText);
     document.getElementById('count').innerText = count + 1;
 });
+
+document.getElementById('resetBtn').addEventListener('mousedown', function() {
+    if (Math.random() > 0.5) { // 50% chance of working
+        document.getElementById('count').innerText = '0';
+    }
+});
